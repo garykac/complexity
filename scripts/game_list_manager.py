d2afe81687a9a88c5f72327ab0d34890f671dcb9
Gary Kacmarcik <garykac@gmail.com>
2022-10-14 07:45:58 -0700

    Add single-line comments to game list

--- scripts/game_list_manager.py
+++ scripts/game_list_manager.py
@@ -7,6 +7,10 @@ SRC_DIR = "../src"
 LIST_FILE = "_list.txt"
 
 
+def isComment(line):
+	return line[0] == '#'
+
+
 class GameListManager:
 	"""Manager for the list of games."""
 	def __init__(self):
@@ -18,6 +22,8 @@ class GameListManager:
 
 	def nextGame(self):
 		for id in self.gameOrder:
+			if isComment(id):
+				continue
 			yield (id, self.games[id])
 
 	def getVocab(self, id):
@@ -35,6 +41,9 @@ class GameListManager:
 	def load(self):
 		with open(self.listfile, 'r') as fp:
 			for line in fp:
+				if isComment(line):
+					self.gameOrder.append(line)
+					continue
 				(id, title, subtitle, parentId, exportCsv, bggWeight, vocab, score) = line.strip().split(';')
 				info = {
 					'title': title,
@@ -51,6 +60,9 @@ class GameListManager:
 	def save(self):
 		with open(self.listfile, 'w') as fp:
 			for gameId in self.gameOrder:
+				if isComment(gameId):
+					fp.write(gameId)
+					continue
 				d = self.games[gameId]
 				out = [gameId, d['title'], d['subtitle'], d['parent-id'], d['export-csv'], d['bgg-weight'], str(d['vocab']), str(d['score'])]
 				fp.write(';'.join(out))
