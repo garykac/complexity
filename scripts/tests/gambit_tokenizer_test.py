013791794694a7d5e29ffad2c9f0633c7b8160ee
Gary Kacmarcik <garykac@gmail.com>
2023-04-02 17:20:27 -0700

    Update tokenizer unit tests

--- scripts/tests/gambit_tokenizer_test.py
+++ scripts/tests/gambit_tokenizer_test.py
@@ -2,36 +2,38 @@ import pytest
 
 from gambit_tokenizer import GambitTokenizer
 
+def checkString(out, expected):
+    assert len(out) == len(expected)
+    for i in range(len(out)):
+    	assert out[i] == expected[i]
+
 def test_tokenizer_simple():
     out = GambitTokenizer.split("Sample string")
-    assert len(out) == 2
+    checkString(out, ["Sample", "string"])
+
     out = GambitTokenizer.split("A sample string with more words")
-    assert len(out) == 6
+    checkString(out, ["A", "sample", "string", "with", "more", "words"])
 
 def test_tokenizer_string():
     out = GambitTokenizer.split('Sample line with "a" string')
-    assert len(out) == 5
-    assert out[3] == '"a"'
+    checkString(out, ["Sample", "line", "with", '"a"', "string"])
 
     out = GambitTokenizer.split('Sample line with "a multi word" string')
-    assert len(out) == 5
-    assert out[3] == '"a multi word"'
+    checkString(out, ["Sample", "line", "with", '"a multi word"', "string"])
 
     out = GambitTokenizer.split('"A string" at the beginning')
-    assert len(out) == 4
-    assert out[0] == '"A string"'
+    checkString(out, ['"A string"', "at", "the", "beginning"])
 
     out = GambitTokenizer.split('At the end, "a string"')
-    assert len(out) == 4
-    assert out[3] == '"a string"'
+    checkString(out, ["At", "the", "end,", '"a string"'])
 
-def test_extractKeyword():
-    checkExtractedKeywords("word", "", "word", "")
-    checkExtractedKeywords("word.", "", "word.", "")
-    checkExtractedKeywords("Word.", "", "Word", ".")
-    
 def checkExtractedKeywords(word, xPre, xWord, xPost):
     out = GambitTokenizer.extractKeyword(word)
     assert out[0] == xPre
     assert out[1] == xWord
     assert out[2] == xPost
+
+def test_extractKeyword():
+    checkExtractedKeywords("word", "", "word", "")
+    checkExtractedKeywords("word.", "", "word.", "")
+    checkExtractedKeywords("Word.", "", "Word", ".")
