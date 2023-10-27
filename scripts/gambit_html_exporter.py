468ee8c595242c1f426487dd5eb05563fe90c328
Gary Kacmarcik <garykac@gmail.com>
2023-10-27 05:14:54 -0700

    Add link on each HTML page to easily file issues

--- scripts/gambit_html_exporter.py
+++ scripts/gambit_html_exporter.py
@@ -43,6 +43,7 @@ class GambitHtmlExporter:
 		out.write('	<link rel="preconnect" href="https://fonts.googleapis.com">\n')
 		out.write('	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>\n')
 		out.write('	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap" rel="stylesheet">\n')
+		out.write('	<script data-file-issue-url="https://github.com/garykac/complexity/issues/new" src="../_file-issue.js" defer></script>\n')
 		out.write('<style>\n')
 
 		with open(GM_CSS_PATH, 'r') as file:
